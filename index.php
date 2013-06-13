bff1849cc42417f1980329fbd91e51740cb099f3
Frits Vlaanderen <frits@hccniet.nl>
2013-06-13 09:48:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,274 @@
+<?php
+ /**********************************\
+ *                                  *
+ *  FLoad                           *
+ *  PHP Filedump script             *
+ *                                  *
+ *   Stolen parts from php.net      *
+ *   Will make it cooler sometime   *
+ *                                  *
+ *   Required:                      *
+ *    - Webserver with X-Sendfile   *
+ *    - PHP (duh)                   *
+ *    - htaccess (or similar) with  *
+ *      mod_rewrite like stuff      *
+ *    - mongodb                     *
+ *                                  *
+ \**********************************/
+
+// TODO: Make more stuff configurable (isn't this just cleanup?)
+//
+// TODO: Rewrite upload part to checksum each chunk, store the checksums in mongodb.
+//        This way we can detect if a similar file already exists before it is fully uploaded.
+//        
+// TODO: Remove writing to tmpfile, write directly to destination
+//
+// TODO: Do something with the statistics in stats collection
+//
+// TODO: rewrite all mongoDB stuff to support $otherDB backends as well
+//
+// TODO: Remove all apache dependency (Done?)
+//        Confirm if I work with $otherWebserver
+//        Webserver needs support for chunking!
+//
+// TODO: CLEANUP!!
+//
+// Search for more TODO's in the rest of this file ;)
+
+
+// We need some configuration
+//   Pass it here, or use an external (ini) file
+//$config['path']      = "/absoulte/path/to/your/data";
+//$config['URL']       = "http://url.com;
+//$config['chunkSize'] = 8192;
+//$config['mongoDb']   = "mongodb_to_put_files_in";
+
+// I use an external file
+$config = parse_ini_file("config.ini");
+
+// This function is a (really) minor modification of Aidan Lister's str_rand()
+function str_rand($length = 8, $seeds = 'alphanum')
+{
+  // Possible seeds
+  $seedings['alpha'] = 'abcdefghijklmnopqrstuvwqyz';
+  $seedings['numeric'] = '0123456789';
+  $seedings['alphanum'] = 'abcdefghijklmnopqrstuvwqyz0123456789';
+  $seedings['Alphanum'] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwqyz0123456789';
+  $seedings['hexidec'] = '0123456789abcdef';
+  
+  // Choose seed
+  if (isset($seedings[$seeds]))
+  {
+    $seeds = $seedings[$seeds];
+  }
+  
+  // Seed generator
+  list($usec, $sec) = explode(' ', microtime());
+  $seed = (float) $sec + ((float) $usec * 100000);
+  mt_srand($seed);
+  
+  // Generate
+  $str = '';
+  $seeds_count = strlen($seeds);
+  
+  for ($i = 0; $length > $i; $i++)
+  {
+    $str .= $seeds{mt_rand(0, $seeds_count - 1)};
+  }
+  
+  return $str;
+}
+
+
+// Create MongoDB connection
+$mc = new Mongo();
+$db = $config['mongoDb'];
+// Select file collection
+$mcFiles = $mc->selectDB($db)->selectCollection("files");
+// Select stats collection
+$mcStats = $mc->selectDB($db)->selectCollection("stats");
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+  // loop over possible random filename
+  while (!isset($newFullName) || !empty($mcResult))
+  {
+    // TODO: Make this more scalable, e.g. possible to auto increase str_rand length when necessary
+    $newFileName = str_rand(3, 'Alphanum');
+    $newFullName = $config['path'] . $newFileName;
+
+    // Search for file in MongoDb
+    $mcQuery = array("filePath" => $newFullName);    
+    $mcResult = $mcFiles->findOne($mcQuery);
+  }
+  
+  // Receive and benefit
+  try
+  {
+    if (!($putData = fopen("php://input", "r")))
+        throw new Exception("What are you putting?");
+  
+    $tot_write = 0;
+    $tmpFileName = tempnam('/tmp/', 'float_');
+    // Create a temp file
+    if (!is_file($tmpFileName))
+    {
+      fclose(fopen($tmpFileName, "x")); //create the file and close it
+      // Open the file for writing
+      if (!($fp = fopen($tmpFileName, "w")))
+        throw new Exception("Can't write to tmp file");
+
+      // Read and write the data, chunked
+      while ($data = fread($putData, $config['chunkSize']))
+      {
+        $chunk_read = strlen($data);
+        if (($block_write = fwrite($fp, $data)) != $chunk_read)
+          throw new Exception("Can't write more to tmp file");
+
+        $tot_write += $block_write;
+      }
+
+      if (!fclose($fp))
+        throw new Exception("Can't close tmp file");
+
+      unset($putData);
+    }
+    else
+    {
+      // Open the file for writing
+      if (!($fp = fopen($tmpFileName, "a")))
+        throw new Exception("Can't write to tmp file");
+  
+      // Read the data a chunk at a time and write to the file
+      while ($data = fread($putData, $config['chunkSize']))
+      {
+        $chunk_read = strlen($data);
+        if (($block_write = fwrite($fp, $data)) != $chunk_read)
+            throw new Exception("Can't write more to tmp file");
+  
+        $tot_write += $block_write;
+      }
+  
+      if (!fclose($fp))
+        throw new Exception("Can't close tmp file");
+  
+      unset($putData);
+    }
+  
+    // Store metadata
+
+    // Check file length and sum
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $file['sha1Sum'] = sha1_file($tmpFileName);
+    $file['fileSize'] = $tot_write;
+    $file['mimeInfo'] = finfo_file($finfo, $tmpFileName);
+    $file['filePath'] = $newFullName;
+    $file['fileUrl'] = $config['URL'] . $newFileName;
+    $file['remoteAddress'] = getenv("REMOTE_ADDR");
+    finfo_close($finfo);
+
+    // Can we talk to our db?
+    if($mc)
+    {
+      // Add results to array if file exists
+      $result = $mcFiles->findOne(array("sha1Sum" => $file['sha1Sum']), array("_id" => 0, "filePath" => 0));
+      if($result)
+      {
+        // We have a file with the same sha1sum
+        $result = array("code" => 0, "msg" => "I already have this file :)", "result" => $result);
+        // And delete the tempfile
+        unlink($tmpFileName);
+      }
+      else
+      {
+        // Unknown file, so lets move it to the pub directory
+        rename($tmpFileName, $newFullName);
+    
+        // insert the metadata in MongoDB
+        $mcFiles->insert($file);
+    
+        // Add old metadata to result array
+        $result = $file;
+        unset($result['filePath']);
+        unset($result['_id']);
+        $result = array("code" => 1, "msg" => "Thank you for your file :)", "result" => $result);
+      }
+      // Report the info to the user
+      echo json_encode($result, JSON_UNESCAPED_SLASHES);
+    }
+    else
+    {
+      // MongoDb connection is not there :o
+      die("Oh my gawwd! tErrOROROR");
+    }
+  }
+  catch (Exception $e)
+  {
+    echo '', $e->getMessage(), "\n";
+  }
+}
+
+// If we get HTTP GET, they'll probably want to download something
+elseif ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+  // let's confirm if a filename is requested
+  if (count($_GET) > 0)
+  {
+    // Filenames should be rewritten and passed as argument
+    if (isset($_GET['file']))
+    {
+      if ($mc)
+      {
+        // Let's see if we have this file
+        $fileName = $_GET['file'];
+        $filePath = $config['path'] . $fileName;
+
+        // Query MongoDb for file info
+        $mcQuery = array("filePath" => $filePath);
+        $mcResult = $mcFiles->findOne($mcQuery);
+
+        // If query is succesful, make sure that we can read the file from disk
+        if (!empty($mcResult) && is_readable($filePath))
+        {
+          // We need the MIME type to pass as http header
+          $fileType = $mcResult["mimeInfo"];
+
+          // Add the X-Sendfile header with filePath so the webserver (mod_xsendfile) can server the file
+          header("X-Sendfile: $filePath");
+          header("Content-Description: File Transfer");
+          header("Content-Type: $fileType");
+          // Let's not force a download dialog, but we can with the following header
+          // header("Content-Disposition: attachment; filename=\"$fileName\"");
+
+          // Let's store this success in DB so we can show off!
+          $fileId = new MongoId($mcResult["_id"]);
+          $mcQuery = array("fileId" => $fileId, "timeStamp" => new MongoDate(), "remoteAddress" => getenv("REMOTE_ADDR"), "remoteUserAgent" => getenv("HTTP_USER_AGENT"));
+          $mcStats->insert($mcQuery);
+
+          exit;
+        }
+        else
+        {
+          header("HTTP/1.0 404 Not Found");
+          echo "No, never heared of $fileName";
+        }
+      }
+      else
+      {
+        // MongoDb connection is not there :o
+        die("Oh my gawwd! tErrOROROR");
+      }
+    }
+    else
+    {
+      // Okay something is clearly wrong
+      echo "WTF MAN!";
+    }
+  }
+  else
+  {
+    // They did a GET request but didn't request a file :/ Maybe they know we do useful stuff like telling them which IP they're using to connect to us
+    printf("%s\n",getenv("REMOTE_ADDR"));
+  }
+}
+?>
