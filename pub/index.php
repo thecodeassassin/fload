66dcf826083a55002b757189f61cfd150b95f881
Stephen Hoogendijk <hoogendijk09@gmail.com>
2014-02-26 15:45:26 +0100

    Update index.php
    removed setting php error reporting

--- pub/index.php
+++ pub/index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(0);
-ini_set('display_errors' , 0);
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
